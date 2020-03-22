fb442d975efffbd48146437eb7565f64a3c763df
jjunsheng <jjunsheng95@gmail.com>
2020-03-23 01:42:45 +0800

    tuples

---
+++ mosh python/tuples.py
@@ -0,0 +1,8 @@
+#tuples, 'top-ples' , item inside tuples cannot be changes
+#perfect for a list of item that name wont be changing
+numbers = (1, 2, 3)
+
+print(numbers.index(2))
+print(numbers.count(2))
+
+numbers[0] = 30
---
+++ mosh python/unpacking.py
@@ -0,0 +1,20 @@
+#unpacking, setting shortcuts for items in list
+
+coordinates = (1, 2, 3)
+test = coordinates[0] * coordinates[1] * coordinates[2]
+print(test)
+
+x = coordinates[0]
+x = coordinates[1]
+x = coordinates[2]
+
+x, y, z = coordinates
+print(x)
+print(y)
+print(z)
+
+geopoint = [4, 5, 6]
+a, b, c = geopoint
+print(a)
+print(b)
+print(c)

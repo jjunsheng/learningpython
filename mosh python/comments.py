28e0830e0490a8df5a43bf3ef6052d21e7ab3b3d
jjunsheng <jjunsheng95@gmail.com>
2020-03-24 23:11:07 +0800

    comments

---
+++ mosh python/comments.py
@@ -0,0 +1,12 @@
+# comments in python to add notes for program
+# example of bad comments : print sky is blue
+# comment get outdated if code change to : ocean is blue, very bad practise
+print("Sky is blue")
+
+
+# calculates and returns the square of the number
+def square (number):
+    return number * number
+
+
+# ^ bad practise also comment is for code below it, for this case is nothing thus getting PEP 8 notice
